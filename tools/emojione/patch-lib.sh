45796c28f5d704f48c702bd979c6e512d038b39f
Danilo Bargen <danilo.bargen@threema.ch>
2017-11-14 11:11:34 +0100

    Add cache busting for Emoji spritemaps

--- tools/emojione/patch-lib.sh
+++ tools/emojione/patch-lib.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 set -e
 BASEPATH=../../public/libs/emojione
+EMOJIONE_VERSION=3.1
+
+# Spritemaps
 sed -i "s/emojione-32-/e1-/g" $BASEPATH/emojione-sprite-32.min.css
+sed -i "s/url(\([^)]*\.png\))/url(\1?v=${EMOJIONE_VERSION})/g" $BASEPATH/emojione-sprite-32.min.css
+
+# Javascript
 sed -i "s/emojione emojione-'+size+\"-\"/e1 e1-'/g" $BASEPATH/emojione.min.js
 sed -i "s/\"emojione/\"e1/g" $BASEPATH/emojione.min.js
