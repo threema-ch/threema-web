cdd6df759635d3ee68b9e3bebc6310857851dded
Danilo Bargen <danilo.bargen@threema.ch>
2018-01-02 09:48:40 +0100

    Happy 2018!

--- header.js
+++ header.js
@@ -1,7 +1,7 @@
 /*
 ** Threema Web.
 **
-** Copyright © 2016-2017 Threema GmbH (https://threema.ch/).
+** Copyright © 2016-2018 Threema GmbH (https://threema.ch/).
 **
 ** This program is free software: you can redistribute it and/or modify
 ** it under the terms of the GNU Affero General Public License as
