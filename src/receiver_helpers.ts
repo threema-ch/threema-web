9851ec9618f9fa294c075057d2fd6a622dbc5e8d
Danilo Bargen <danilo.bargen@threema.ch>
2018-06-13 16:42:49 +0200

    Add receiver helpers module

--- src/receiver_helpers.ts
+++ src/receiver_helpers.ts
@@ -1,6 +1,4 @@
 /**
- * Copyright © 2016-2018 Threema GmbH (https://threema.ch/).
- *
  * This file is part of Threema Web.
  *
  * Threema Web is free software: you can redistribute it and/or modify it
@@ -17,9 +15,19 @@
  * along with Threema Web. If not, see <http://www.gnu.org/licenses/>.
  */
 
-// tslint:disable:no-reference
-/// <reference path="../../src/threema.d.ts" />
+// This file contains helper functions related to receivers.
+// Try to keep all functions pure!
 
-import './containers';
-import './helpers';
-import './receiver_helpers';
+/**
+ * Return wether a contact is a Threema Gateway contact.
+ */
+export function isGatewayContact(receiver: threema.ContactReceiver) {
+    return receiver.id.startsWith('*');
+}
+
+/**
+ * Return wether a contact is the ECHOECHO test contact.
+ */
+export function isEchoContact(receiver: threema.ContactReceiver) {
+    return receiver.id === 'ECHOECHO';
+}
