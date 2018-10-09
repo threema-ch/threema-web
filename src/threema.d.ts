a9b06020c11dbeeefa098e56420c09db944febe7
Danilo Bargen <danilo.bargen@threema.ch>
2018-10-09 11:03:01 +0200

    Next release name: Glärnisch

--- src/threema.d.ts
+++ src/threema.d.ts
@@ -621,6 +621,7 @@ declare namespace threema {
         VERSION_MOUNTAIN: string;
         VERSION_MOUNTAIN_URL: string;
         VERSION_MOUNTAIN_IMAGE_URL: string;
+        VERSION_MOUNTAIN_IMAGE_COPYRIGHT: string;
         VERSION_MOUNTAIN_HEIGHT: number;
         GIT_BRANCH: string;
         SALTYRTC_PORT: number;
