5d76b5e978f1318ad5a48d630482e6372aad9beb
Lennart Grahl <lennart.grahl@gmail.com>
2018-10-18 13:11:27 +0200

    Minimum delay futures (#629)
    Replace ExecuteService with an extension to the Future class

--- public/libs/future.js
+++ public/libs/future.js
@@ -21,6 +21,31 @@ class Future extends Promise {
     }
 
     /**
+     * Wrap a promise to ensure it does not resolve before a minimum
+     * duration.
+     *
+     * Note: The promise will still reject immediately. Furthermore, be
+     *       aware that the promise does not resolve/reject inside of
+     *       an AngularJS digest cycle.
+     *
+     * @param promise the promise or future to be wrapped
+     * @param minDuration the minimum duration before it should be resolved
+     * @returns {Future}
+     */
+    static withMinDuration(promise, minDuration) {
+        const start = new Date();
+        return new Future((resolve, reject) => {
+            promise
+                .then((result) => {
+                    const timediff = new Date() - start;
+                    const delay = Math.max(minDuration - timediff, 0);
+                    self.setTimeout(() => resolve(result), delay);
+                })
+                .catch((error) => reject(error));
+        });
+    }
+
+    /**
      * Return whether the future is done (resolved or rejected).
      */
     get done() {
