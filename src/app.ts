dfdf175bb9790e32f0941db63ad32db87a39b3a1
Danilo Bargen <danilo.bargen@threema.ch>
2018-10-18 11:13:11 +0200

    Fix Safari detection for showing timestamps

--- src/app.ts
+++ src/app.ts
@@ -144,7 +144,7 @@ angular.module('3ema', [
     function($log: ng.ILogService, CONFIG: threema.Config, browserService: BrowserService) {
         // For Safari (when in DEBUG mode), monkey-patch $log to show timestamps.
 
-        if (!(CONFIG.DEBUG && browserService.getBrowser().isSafari)) {
+        if (!(CONFIG.DEBUG && browserService.getBrowser().isSafari(false))) {
             return;
         }
 
