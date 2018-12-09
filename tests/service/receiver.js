45d66e6b1017df0b35c5688d27f727776c22d327
IndianaDschones <IndianaDschones@users.noreply.github.com>
2018-12-09 11:44:37 +0100

    Added unit test for isBlocked function

---
+++ tests/service/receiver.js
@@ -0,0 +1,68 @@
+describe('ReceiverService', function () {
+
+    let $service;
+
+    // Ignoring page reload request
+    beforeAll(() => window.onbeforeunload = () => null);
+
+    beforeEach(function () {
+
+        module('3ema.services');
+
+        // Inject the service
+        inject(function (ReceiverService) {
+            $service = ReceiverService;
+        });
+
+    });
+
+    describe('Receiver', () => {
+        it('is not blocked', () => {
+            expect($service.isBlocked({
+                id: 'FOOOOBAR',
+                type: 'contact',
+                isBlocked: false
+            })).toEqual(false);
+        });
+        it('is blocked', () => {
+            expect($service.isBlocked({
+                id: 'FOOOOBAR',
+                type: 'contact',
+                isBlocked: true
+            })).toEqual(true);
+        });
+    });
+
+    describe('Invalid receiver', () => {
+        it('group is not blocked', () => {
+            expect($service.isBlocked({
+                id: 'FOOOOBAR',
+                type: 'group',
+                isBlocked: false
+            })).toEqual(false);
+        });
+        it('invalidType is not blocked', () => {
+            expect($service.isBlocked({
+                id: 'FOOOOBAR',
+                type: 'invalidType',
+                isBlocked: true
+            })).toEqual(false);
+        });
+    });
+
+    describe('Receiver without isBlocked flag', () => {
+        it('is not blocked', () => {
+            expect($service.isBlocked({
+                id: 'FOOOOBAR',
+                type: 'contact'
+            })).toEqual(false);
+        });
+        it('is blocked', () => {
+            expect($service.isBlocked({
+                id: 'FOOOOBAR',
+                type: 'invalidType'
+            })).toEqual(false);
+        });
+    });
+
+});
