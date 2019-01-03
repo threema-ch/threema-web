1452ae94cec46f13e888a3e80e2ce54ee552bf3f
Danilo Bargen <danilo.bargen@threema.ch>
2019-01-03 16:21:37 +0100

    Use explicit targets for babel-preset-env
    This reduces bundle size by 5 KiB.

--- dist/babelify-config.js
+++ dist/babelify-config.js
@@ -2,6 +2,12 @@ const babelifyConfig = {
     presets: [
         ['@babel/preset-env', {
             'useBuiltIns': 'entry',
+            'targets': {
+                'firefox': 50,
+                'chrome': 45,
+                'opera': 32,
+                'safari': 11,
+            },
         }],
     ],
     extensions: '.ts',
