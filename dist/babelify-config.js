53bf1573cd51b46a57d960053ecec4406778b2d5
Danilo Bargen <danilo.bargen@threema.ch>
2019-01-07 11:06:23 +0100

    Use babel-plugin-transform-runtime

--- dist/babelify-config.js
+++ dist/babelify-config.js
@@ -1,13 +1,21 @@
 const babelifyConfig = {
     presets: [
         ['@babel/preset-env', {
-            'useBuiltIns': 'entry',
-            'targets': {
-                'firefox': 60,
-                'chrome': 65,
-                'opera': 52,
-                'safari': 11,
+            useBuiltIns: 'entry',
+            targets: {
+                firefox: 60,
+                chrome: 65,
+                opera: 52,
+                safari: 11,
             },
+        }],
+    ],
+    plugins: [
+        ['@babel/plugin-transform-runtime', {
+            corejs: false,
+            helpers: true,
+            regenerator: true,
+            useESModules: false,
         }],
     ],
     extensions: '.ts',
