6045cb7cdcdc9d89c7d8e914d65c2f24c2d2a137
Danilo Bargen <mail@dbrgn.ch>
2019-01-17 14:07:03 +0100

    Fix budo config (with a workaround for now) (#705)

--- dist/devserver.js
+++ dist/devserver.js
@@ -7,14 +7,21 @@
 const budo = require('budo')
 const babelify = require('babelify')
 const babelifyConfig = require('./babelify-config.js');
+const tsify = require('tsify');
 
-budo(null, {
+budo('src/app.ts:dist/app.js', {
     dir: ['public', '.', 'src'],
     live: true,
     stream: process.stdout,
     port: 9966,
     debug: true,
-    browserify: {
-        transform: [babelify, babelifyConfig]
-    },
+    //browserify: {  // TODO: See #705
+    //    plugin: tsify,
+    //    transform: [babelify.configure(babelifyConfig)],
+    //},
+    browserifyArgs: ['-d', '-p', 'tsify', '-t', '[', 'babelify', '--presets', '[', '@babel/env', ']', '--extensions', '.ts', ']'],
+}).on('connect', (ev) => {
+    console.log('Server running on %s', ev.uri);
+}).on('update', (buffer) => {
+    console.log('Bundle update, %d bytes', buffer.length);
 });
