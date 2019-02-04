eb9a5c30ebd9bb3be7664c0420044073b6f13c28
Lennart Grahl <lennart.grahl@gmail.com>
2019-02-04 11:25:03 +0100

    Introduce linting for SASS/CSS (#525)
    Pedantic cleanup:
    
    - Add SASS / SCSS linting
    - Add autofix for SASS linter & update configuration
    - Fix various CSS properties that are redundant, get overwritten by another property, are arcane or simply never existed
    - Remove arcane/irrelevant CSS vendor prefixes
    - Fix div not allowed inside h1
    - Fix h1 is already a block element and (#logo is a child)
    - Adjust some line breaks and comment style for consistency
    - Introduce "concentric" property sort order
    - Lots of stylistic changes to SCSS files

--- src/directives/compose_area.ts
+++ src/directives/compose_area.ts
@@ -753,7 +753,7 @@ export default [
                                 offset = pos;
                                 break;
                             case Node.ELEMENT_NODE:
-                                size = getOuterHtml(node).length ;
+                                size = getOuterHtml(node).length;
                                 break;
                             default:
                                 $log.warn(logTag, 'Unhandled node:', node);
