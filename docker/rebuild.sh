06a3d2748298d2b5c6b3330c39955aa43b9ddeec
Danilo Bargen <danilo.bargen@threema.ch>
2019-02-28 15:42:00 +0100

    Fix docker/rebuild.sh script

--- docker/rebuild.sh
+++ docker/rebuild.sh
@@ -21,25 +21,37 @@ else
     DOCKER="docker"
 fi
 
-echo "Logging in:"
+echo "Logging in..."
 $DOCKER login -u $DOCKER_USER -p $DOCKER_API_KEY
+
+echo "Create temporary directory..."
+mkdir builds
 
 echo -e "\nBuilding branches:"
 for branch in $SUPPORTED_BRANCH_NAMES; do
     echo -e "\n- $branch"
+    git clone -b $branch . "builds/$branch"
+    cd "builds/$branch"
     $DOCKER build . --no-cache -t $IMAGE_NAME:$branch
+    $DOCKER push $IMAGE_NAME:$branch
+    cd ../..
+    rm -rf "builds/$branch"
 done
 
 echo -e "\nBuilding tags:"
 for pattern in $SUPPORTED_TAG_PATTERNS; do
     for tag in $(git tag | grep $pattern); do
         echo -e "\n- $tag"
+        git clone -b $tag . "builds/$tag"
+        cd "builds/$tag"
         maintag=$tag
         minortag=$(echo $tag | sed 's/^\(v[0-9]*\.[0-9]*\)\..*$/\1/')
         majortag=$(echo $tag | sed 's/^\(v[0-9]*\)\..*$/\1/')
-        $DOCKER build . --no-cache -t $IMAGE_NAME:$tag -t $IMAGE_NAME:$minortag -t threema/threema
+        $DOCKER build . --no-cache -t $IMAGE_NAME:$tag -t $IMAGE_NAME:$minortag -t $IMAGE_NAME:$majortag
+        $DOCKER push $IMAGE_NAME:$tag
+        $DOCKER push $IMAGE_NAME:$minortag
+        $DOCKER push $IMAGE_NAME:$majortag
+        cd ../..
+        rm -rf "builds/$tag"
     done
 done
-
-echo -e "\nPushing image with all tags"
-$DOCKER push $IMAGE_NAME
