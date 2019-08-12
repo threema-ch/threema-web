b1ad91f552f93743f5729cbbcfd125ace2522bb0
Danilo Bargen <danilo.bargen@threema.ch>
2019-08-12 10:49:22 +0200

    Show warning if browser does not support TextDecoder

--- public/js/compat.js
+++ public/js/compat.js
@@ -1,7 +1,19 @@
+// WebAssembly
 if (typeof WebAssembly === "object" && typeof WebAssembly.instantiate === "function") {
     console.debug('Compat: WebAssembly is supported');
 } else {
     console.error('Compat: WebAssembly is NOT supported');
     // Show warning
-    document.querySelector('.compat.wasm').style = 'inherit';
+    document.querySelector('.compat.compat-js').style.display = 'inherit';
+    document.querySelector('.compat.compat-js .wasm').style.display = 'inherit';
+}
+
+// TextDecoder
+if (typeof TextDecoder !== "undefined") {
+    console.debug('Compat: TextDecoder is supported');
+} else {
+    console.error('Compat: TextDecoder is NOT supported');
+    // Show warning
+    document.querySelector('.compat.compat-js').style.display = 'inherit';
+    document.querySelector('.compat.compat-js .textdecoder').style.display = 'inherit';
 }
