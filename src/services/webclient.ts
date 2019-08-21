adfb52a33d92a1048964e264c75c070c9cee46bf
Lennart Grahl <lennart.grahl@gmail.com>
2019-08-21 15:25:25 +0200

    Do not trigger a protocol error when the data channel closes

--- src/services/webclient.ts
+++ src/services/webclient.ts
@@ -1095,8 +1095,7 @@ export class WebClientService {
                 });
             };
             dc.onclose = () => {
-                this.arpLog.error(`Data channel ${dc.label} closed prematurely`);
-                this.failSession();
+                this.arpLog.info(`Data channel ${dc.label} closed`);
             };
             dc.onerror = (event) => {
                 this.arpLog.error(`Data channel ${dc.label} error:`, event);
