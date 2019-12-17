e6a3d565f25257e401d0d1bf022b9845cd89526b
Danilo Bargen <danilo.bargen@threema.ch>
2019-12-17 09:26:19 +0100

    Webpack config: Remove outdated comment

--- webpack.common.js
+++ webpack.common.js
@@ -1,10 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
 
-// Target a version without class support due to this issue:
-// https://stackoverflow.com/q/43307607
-// This does not mean that we support Firefox <60ESR!
-// We will increase the target again once FF52 has died out.
 const babelOptions = {
   presets: [
     ['@babel/preset-env', {
