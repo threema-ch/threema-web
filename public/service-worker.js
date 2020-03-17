7bb2ba0e536613993acd40afcf8c1d8008595732
Christoph Schwizer <c.schwizer.dev@gmail.com>
2020-03-17 15:14:46 +0100

    Add service worker for PWA (#972)
    Fix resolution mismatch in manifest.webmanifest
    Add service worker
    Embed service worker registration in `src/bootstrap.ts` and enable console output

---
+++ public/service-worker.js
@@ -0,0 +1,16 @@
+// service worker for PWA (progressive web app)
+
+self.addEventListener('install', (event) => {
+    // console.log('👷', 'install', event);
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    // console.log('👷', 'activate', event);
+    return self.clients.claim();
+});
+
+self.addEventListener('fetch', function (event) {
+    // console.log('👷', 'fetch', event);
+    event.respondWith(fetch(event.request));
+});
