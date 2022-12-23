a7a823963871c74f291a706017bdf74b4992ef25
Danilo Bargen <danilo.bargen@threema.ch>
2022-12-23 12:01:52 +0100

    Add title tag with names to group reactions

--- src/directives/message_group_reactions.ts
+++ src/directives/message_group_reactions.ts
@@ -23,8 +23,7 @@ import {WebClientService} from '../services/webclient';
  * Create the appropriate icon for the state of the specified message.
  */
 export default [
-    'WebClientService',
-    function(webClientService: WebClientService) {
+    function() {
         return {
             restrict: 'EA',
             scope: {},
@@ -32,25 +31,40 @@ export default [
                 message: '=eeeMessage',
             },
             controllerAs: 'ctrl',
-            controller: [function() {
+            controller: ['WebClientService', '$filter', function(webClientService: WebClientService, $filter: ng.IFilterService) {
+                const idsToNames: (ids: string[], sort: boolean) => string = $filter('idsToNames');
                 this.$onInit = function() {
                     const message = (this.message as threema.Message);
                     this.reactions = {
-                        up: message.reactions?.ack?.length ?? 0,
-                        down: message.reactions?.dec?.length ?? 0,
+                        up: {
+                            count: message.reactions?.ack?.length ?? 0,
+                            names: '',
+                            icon: (message.reactions?.ack ?? []).includes(webClientService.me.id) ? 'thumb_up' : 'thumb_up_off_alt',
+                        },
+                        down: {
+                            count: message.reactions?.dec?.length ?? 0,
+                            names: message.reactions?.dec?.sort()?.join(', ') ?? '',
+                            icon: (message.reactions?.dec ?? []).includes(webClientService.me.id) ? 'thumb_down' : 'thumb_down_off_alt',
+                        },
                     }
-                    this.iconUp = (message.reactions?.ack ?? []).includes(webClientService.me.id) ? 'thumb_up' : 'thumb_up_off_alt';
-                    this.iconDown = (message.reactions?.dec ?? []).includes(webClientService.me.id) ? 'thumb_down' : 'thumb_down_off_alt';
+                    const upvoters = message.reactions?.ack ?? [];
+                    const downvoters = message.reactions?.dec ?? [];
+                    if (upvoters.length > 0) {
+                        this.reactions.up.names = idsToNames(upvoters, true);
+                    }
+                    if (downvoters.length > 0) {
+                        this.reactions.down.names = idsToNames(downvoters, true);
+                    }
                 }
             }],
             template: `
-                <span class="reaction-ack" ng-if="ctrl.reactions.up > 0">
-                    <i class="material-icons md-14">{{ctrl.iconUp}}</i>
-                    {{ctrl.reactions.up}}
+                <span class="reaction-ack" ng-if="ctrl.reactions.up.count > 0" title="{{ctrl.reactions.up.names}}">
+                    <i class="material-icons md-14">{{ctrl.reactions.up.icon}}</i>
+                    {{ctrl.reactions.up.count}}
                 </span>
-                <span class="reaction-dec" ng-if="ctrl.reactions.down > 0">
-                    <i class="material-icons md-14">{{ctrl.iconDown}}</i>
-                    {{ctrl.reactions.down}}
+                <span class="reaction-dec" ng-if="ctrl.reactions.down.count > 0" title="{{ctrl.reactions.down.names}}">
+                    <i class="material-icons md-14">{{ctrl.reactions.down.icon}}</i>
+                    {{ctrl.reactions.down.count}}
                 </span>
             `,
         };
