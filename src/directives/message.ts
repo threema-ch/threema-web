02e95d90f367535a5e8527ffeab041a3776a6fb6
Lenny <lennart.grahl@threema.ch>
2024-12-04 17:52:28 +0100

    Minimal emoji reactions support

--- src/directives/message.ts
+++ src/directives/message.ts
@@ -90,7 +90,10 @@ export default [
                     this.showText = this.message.type === 'text' || this.message.caption;
                     this.showMedia = this.message.type !== 'text';
                     this.showState = messageService.showStatusIcon(this.message as threema.Message, this.receiver);
-                    this.showGroupReactions = this.isGroup && webClientService.appCapabilities.groupReactions;
+                    this.showGroupReactions =
+                        !webClientService.appCapabilities.emojiReactions && this.isGroup
+                        && webClientService.appCapabilities.groupReactions;
+                    this.showEmojiReactions = webClientService.appCapabilities.emojiReactions;
                     this.showQuote = this.message.quote !== undefined;
                     this.showVoipInfo = this.message.type === 'voipStatus';
 
